ba2941c426e50a93d1bb51840ee6600ede624c36
BrazilPepe <labancz.marton@gmail.com>
2017-02-23 12:51:14 +0100

    Vége az órának

--- ConsoleApplication5/Program.cs
+++ ConsoleApplication5/Program.cs
@@ -10,6 +10,14 @@ namespace ConsoleApplication5
     {
         static void Main(string[] args)
         {
+            C c = new ConsoleApplication5.C();
+            //s1, s2, s5, v2, s6, s7
+            // c.S1(); 
+            //c.S2();
+            //c.S5();
+
+            c.S5();
+            Console.ReadKey();
         }
     }
 }
